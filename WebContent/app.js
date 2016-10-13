30a72e324a9b4aee3c0f79760e3c94a0244adceb
vj <vnetijava@gmail.com>
2016-10-13 12:14:46 -0700

    module 2 solution

---
+++ WebContent/app.js
@@ -0,0 +1,42 @@
+(function (){
+	'use strict';
+	
+	var shoppingList = [
+		{
+			name:" Milk ",
+			quantity : "3 Gallons"
+			
+		},
+		{
+			name : "Chips",
+			quantity:" 3 bags"
+		},
+		{
+			name:"Soda",
+			quantity:"1 case"
+		}
+	];
+	var boughtItemList=[];
+	
+angular.module('ShoppingListApp',[])
+		.controller('ShoppingListController',ShoppingListController);
+	
+		ShoppingListController.$inject = ['$scope'];
+		
+		function ShoppingListController($scope){
+			$scope.shoppingList = shoppingList
+			$scope.boughtItemList = boughtItemList;
+			
+			$scope.updateList = function(itemIndex){
+				var item = shoppingList[itemIndex];
+		
+				var newItem ={
+						name:item.name,
+						quantity:item.quantity
+			
+				};
+				$scope.boughtItemList.push(newItem);
+				$scope.shoppingList.splice(itemIndex,1);
+			}
+		}
+})();
